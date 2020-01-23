ee5b6dfffa9b8e77f927812bab3fba1578451415
Corey Sherman <corey.sherman@accesso.com>
2020-01-23 12:01:20 -0500

    remove dist

--- dist/date-range-picker.module.d.ts
+++
@@ -1,6 +0,0 @@
-import { ModuleWithProviders } from '@angular/core';
-import { LocaleConfig } from './date-range-picker.config';
-export declare class NgxDateRangePickerMd {
-    constructor();
-    static forRoot(config?: LocaleConfig): ModuleWithProviders;
-}
--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { NgxDateRangePickerMd } from './date-range-picker.module';
-export { DateRangePickerComponent } from './components/date-range-picker.component';
-export { DateRangePickerDirective } from './directives/date-range-picker.directive';
-export { LocaleConfig, LOCALE_CONFIG, DefaultLocaleConfig } from './date-range-picker.config';
-export { LocaleService } from './services/locale.service';
