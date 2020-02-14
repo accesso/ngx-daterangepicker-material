522e84dbe1ff1d61ab5a2ee7bc51f345df078664
Corey Sherman <corey.sherman@accesso.com>
2020-02-14 12:42:31 -0500

    made updates to make the component testable in cypress

--- date-range-picker.models.d.ts
+++ date-range-picker.models.d.ts
@@ -1,5 +1,6 @@
 import * as moment from 'moment';
 export interface DateRangePreset {
+    key: string;
     label: string;
     range: {
         start: moment.Moment;
--- src/daterangepicker/date-range-picker.models.ts
+++ src/daterangepicker/date-range-picker.models.ts
@@ -1,6 +1,7 @@
 import * as moment from 'moment';
 
 export interface DateRangePreset {
+	key: string;
 	label: string;
 	range: {
 		start: moment.Moment,
